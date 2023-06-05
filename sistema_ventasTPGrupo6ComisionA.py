fbe510b5775df183ceefe1a563a6c5adefc04bc1
ojdaelias <eli24ojeda@gmail.com>
2023-06-05 10:40:38 -0300

    2.0

---
+++ sistema_ventasTPGrupo6ComisionA.py
@@ -0,0 +1,83 @@
+#-------------------------------Definicion de funciones-------------------------------#
+#Menu
+def Menu():
+    x = int(input('''Seleccione la opcion que desee realizar:
+    1. Cargar los datos del archivo "locales-en-venta-2020.csv"
+    2. Generar estadísticas. Debe desplegar un submenú únicamente al escoger la opción mostrando las 
+    diferentes opciones de resultados a generar.
+    3. Agregar registros de nuevos locales.
+    4. Actualizar los datos en el archivo locales-en-venta-2020.csv
+    5. Salir
+    Ingrese el numero de la opcion que quiera seleccionar'''))
+    return x
+#Submenu
+def subMenu():
+    x = int(input('''Seleccione la opcion que desee realizar:
+    1. Generar el archivo semestre1.csv que contenga los datos de locales en venta de los trimestres 
+    PRIMER y SEGUNDO
+    2. Generar el archivo galeriaEstrenar.csv que contenga los datos de locales en venta en galerías, y 
+    con antigüedad 0.
+    3. Mostrar los locales en venta con mayor precio en cada barrio
+    4. Generar un archivo con el promedio de superficies de locales con más de 10 años de 
+    antigüedad
+    Ingrese el numero de la opcion que quiera seleccionar'''))
+    return x
+#Agregar datos
+def agregarRegistros():
+    dato = superficie
+
+    input(f"Ingrese {dato}")
+    trimestre = ['primer','segundo']
+    cotizacion = 
+
+    ing
+#-------------------------------Programa principal-------------------------------#
+while Menu() != 5: 
+    if Menu() == 1:
+
+    elif Menu() == 2:
+        if matriz == []:
+            print('Debes cargar los datos antes de trabajar con ellos' + Menu(), sep='\n')
+        if subMenu() == 1:
+        elif subMenu() == 2:
+        elif subMenu() == 3:
+        elif subMenu() == 4:
+    elif Menu() == 3:
+    elif Menu() == 4:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----------------------AGREGAR REGISTROS DE NUEVOS LOCALES----------------------#
+
+    
+
